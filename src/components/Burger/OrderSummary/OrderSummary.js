eb1673ce6287a3c21c1114821fbbe3eed804aa59
narges-safari <narges.safari.8631@gmail.com>
2019-09-26 11:09:43 +0330

    burger-builder

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import Auxx from '../../../hoc/Auxx/Auxx';
+import Button from '../../UI/Button/Button'; 
+
+class OrderSummary extends Component {
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igkey =>{
+            return (
+                <li key={igkey}>
+                    <span style={{textTransform: 'capitalize'}}>{igkey}</span>: {this.props.ingredients[igkey]}
+                </li>)
+        })
+        return(
+            <Auxx>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price}</strong></p>
+                <p>continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        </Auxx>
+        );
+    }
+}
+
+export default OrderSummary;
